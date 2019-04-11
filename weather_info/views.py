dc2f89d06e735b093ba55a2fd8b843a756e6f1f5
hyojin1912 <chaosjin1912@gmail.com>
2019-04-11 16:34:36 +0900

    네이버 날씨정보 mail 보내기 (host:8888/weather_info)

--- weather_info/views.py
+++ weather_info/views.py
@@ -9,62 +9,36 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from time import sleep
 import json
-
-@csrf_exempt
-def weather_info(name):
-    print("weather_info 호출")
-    # url = 'https://search.naver.com/search.naver?sm=top_hty&fbm=0&ie=utf8&query='+str(name)
-    # headers = {
-    #     'user-agent' : 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
-    # }
-    # try:
-    #     response = requests.get(url, headers=headers)
-    #     if(response.status_code != 200):
-    #         response.raise_for_status()
-    # except HTTPError:
-    #     return JsonResponse({'message' : 'Error at crawling' + name})
-    # html = response.text
-    # soup = BeautifulSoup(html, 'html.parser')
-    # list = soup.select('.biz_name')
-    # list2 = soup.select('#sp_local_1 > div.tit_area > a > strong')
-    # list3 = soup.select('#sp_local_1 > dl > dt > a')
-    # if (not list) and (not list2) and (not list3):
-    #     sleep(0.5)
-    #     return name
-    # else:
-    #     sleep(0.5)
-    #     return 'open'
+import smtplib, ssl
 
 @csrf_exempt
 def main(request):
-    print("test")
-    # rest_response = requests.get('https://devbotfood.jellylab.io:6001/api/v1/users/get_all_restaurant')
-    # rest = json.loads((rest_response.text))
-    # print(rest)
-    # p = Pool(3)
-    # result = p.map(verify_close, rest)
-    # p.close()
-    # p.join()
-    # # result = map(verify_close, rest)
-    # print(result)
-    # result = [x for x in result if x != 'open']
-    # print(result)
-    # for i in result:
-    #     print(i)
-    #     subway, res_name = i.split(None,maxsplit=1)
-    #     query_header = {
-    #     'Content-Type': 'application/json'}
-    #     data = {"res_name" : res_name,"subway" : subway}
-    #     try:
-    #         query_response = requests.post('https://devbotfood.jellylab.io:6001/api/v1/users/update_closedown', headers=query_header, data=json.dumps(data))
-    #         if(query_response.status_code != 200):
-    #             query_response.raise_for_status()
-    #     except HTTPError:
-    #         return JsonResponse({'message' : 'Error at updating query' + subway + res_name})
-    #     sleep(0.5)
+
+    # 날씨정보 크롤링
+    url = 'https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EC%84%9C%EC%9A%B8+%EB%82%A0%EC%94%A8&oquery=%EC%9B%90%EC%A3%BC+%EB%82%A0%EC%94%A8&tqi=U6xA5dp0JXVssA9WTuGssssstmZ-432536'
+    res = requests.get(url)
+    html = res.text
+    soup = BeautifulSoup(html, 'html.parser')
+    found = soup.find("div",{"class": "sc cs_weather _weather"})
+    found2 = found.find("div", {"class": "info_data"})
+    found3 = found2.find("p", {"class": "cast_txt"})
+    wt_summary = found3.text
+
+    # 파이썬 메일 보내기 템플릿
+    port = 465  # For SSL
+    senderEmail = "chaosjin1912@gmail.com"
+    password = input("Type your password and press enter: ")
+    subject = "python mail test"
+    message = 'Subject: {}\n\n{}'.format(subject, wt_summary).encode('utf-8')
+
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login("chaosjin1912@gmail.com", password)
+        server.sendmail("chaosjin1912@gmail.com", "chaosjin1912@gmail.com", message)
+
     return JsonResponse({'message' : 'success'})
 
-
-#
 # if __name__ == '__main__':
 #     main()
